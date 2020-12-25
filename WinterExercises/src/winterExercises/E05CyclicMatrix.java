fe89a9f912943b4e94d5261fdb97f0c35090451c
antonio210 <73605423+antonio210@users.noreply.github.com>
2020-12-25 12:03:44 +0100

    Update E05CyclicMatrix.java
    Adding 'while' loop to remove repetitive 'for' loops. Changed direction.
    
    Print is now easier to understand.

--- WinterExercises/src/winterExercises/E05CyclicMatrix.java
+++ WinterExercises/src/winterExercises/E05CyclicMatrix.java
@@ -1,67 +1,48 @@
 package winterExercises;
+
 public class E05CyclicMatrix {
-	
+
 	public static void main(String[] args) {
-		int size = 5; int xz = size-1; int yz = size-1;
-		 
-		int xa= 0; int ya= 0;
-		
-		int num = 1; 
-	
+		int size = 5;
+		int xz = size - 1;
+		int yz = size - 1;
+
+		int xa = 0;
+		int ya = 0;
+
+		int num = 1;
+
 		int[][] table = new int[size][size];
-	
-		for(int i=xz;i>=xa;i-- ) {
-			table[i][yz]= num++;
+
+		while (num <= (size * size)) {
+			for (int i = yz; i >= ya; i--) {
+				table[xz][i] = num++;
+			}
+			xz--;
+
+			for (int i = xz; i >= xa; i--) {
+				table[i][ya] = num++;
+			}
+			ya++;
+
+			for (int i = ya; i <= yz; i++) {
+				table[xa][i] = num++;
+			}
+			xa++;
+
+			for (int i = xa; i <= xz; i++) {
+				table[i][yz] = num++;
+			}
+			yz--;
 		}
 
-		yz--;
-		for(int j = yz;j>=ya;j--) {
-			table[xa][j]=num++;
+		for (int i = 0; i < table.length; i++) {
+			for (int j = 0; j < table[i].length; j++) {
+				System.out.print((table[i][j] >= 10 ? table[i][j] : ("0" + table[i][j])) + " ");
+			}
+			System.out.println();
 		}
-		xa++;
-		
-		for(int i = xa;i<=xz;i++) {
-			table[i][ya]=num++;
-		}
-		ya++;
-		
-		for(int i =ya;i<=yz;i++) {
-			table[xz][i]=num++;
-		}
-		xz--;
-		
-		for(int i=xz;i>=xa;i-- ) {
-			table[i][yz]= num++;
-		}
-		
-		yz--;
-		for(int j = yz;j>=ya;j--) {
-			table[xa][j]=num++;
-		}
-		xa++;
-		
-		for(int i = xa;i<=xz;i++) {
-			table[i][ya]=num++;
-		}
-		ya++;
-		
-		for(int i =ya;i<=yz;i++) {
-			table[xz][i]=num++;
-		}
-		xz--;
-	
-		for(int i=xz;i>=xa;i-- ) {
-			table[i][yz]= num++;
-		}
-	
-		for (int i = 0; i < table.length; i++) { 
-	         for (int j = 0; j < table[i].length; j++) {
-	            System.out.print(table[i][j] + " ");
-	         }
-	         System.out.println();
-	      }
-		
-		
+
 	}
 
 }
